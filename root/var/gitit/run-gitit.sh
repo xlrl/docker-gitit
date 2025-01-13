5a19d1313c498bebd332fb4fb80045aa4d67bcd4
Alexander Mueller <alexander.mueller@littleblue.de>
2025-01-13 19:19:51 +0100

    Fix startup script

--- root/var/gitit/run-gitit.sh
+++ root/var/gitit/run-gitit.sh
@@ -1,3 +1,9 @@
 #/bin/sh
-chmod -R gitit:gitit /gitit
-su gitit -c gitit -p 4000
+set -e
+chown -R gitit:gitit /gitit
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
+# for whatever reason haskell doesn't like the locale
+export LANG=C.UTF-8
+cd /gitit
+su gitit -c "gitit -p 4000"
